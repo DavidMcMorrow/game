2dcacd6653394ea16b32bef992b414d0c199ca5f
DavidMcMorrow <mcmorrda@tcd.ie>
2024-02-11 17:10:45 +0000

    basic mechanics

--- game/main.py
+++ game/main.py
@@ -1,12 +1,36 @@
 import pygame as py
+
+def createCircle():
+    SCREEN.fill((0, 0, 255))
+
+def gravity(circle_y, g):
+    if(circle_y < 350):
+        circle_y = circle_y + g
+    return circle_y
+
+def jump(circle_y, jump): 
+    return circle_y - jump
+
+
 size = (500, 400)
-screen = py.display.set_mode(size)
+SCREEN = py.display.set_mode(size)
+
+circle_y = 50
+GRAVITYDISTANCE = 0.04
+JUMPDISTANCE = 40
+
 while True:
     for ev in py.event.get():
-        if(ev.type == py.MOUSEBUTTONUP):
-            pos = py.mouse.get_pos()
-            col = (0, 255, 255)
-            py.draw.circle(screen, col, pos, 20, 6)
-            py.display.update()
         if ev.type == py.QUIT:
             py.display.quit()
+        if ev.type == py.KEYDOWN:
+            circle_y = jump(circle_y, JUMPDISTANCE)
+    
+    createCircle()
+    circle_y = gravity(circle_y, GRAVITYDISTANCE)
+
+    py.draw.ellipse(SCREEN, (255, 255, 0), (50, circle_y, 50, 50))
+    py.display.update()
+
+
+
