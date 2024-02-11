ffbe1a53e1decd87d6295aba80933eb930a6971a
DavidMcMorrow <mcmorrda@tcd.ie>
2024-02-11 16:27:33 +0000

    got the environment set up, with basic click functions

--- game/main.py
+++ game/main.py
@@ -1,2 +1,12 @@
-print('Hello World')
-print("testing")
+import pygame as py
+size = (500, 400)
+screen = py.display.set_mode(size)
+while True:
+    for ev in py.event.get():
+        if(ev.type == py.MOUSEBUTTONUP):
+            pos = py.mouse.get_pos()
+            col = (0, 255, 255)
+            py.draw.circle(screen, col, pos, 20, 6)
+            py.display.update()
+        if ev.type == py.QUIT:
+            py.display.quit()
